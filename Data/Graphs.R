4e21c89a1b8657e7430484b6094e9d5bc1671965
unknown <aatu nieminen>
2020-03-16 19:31:01 -0500

    Created Deaths by state graphs. Created top ten States by Death. Created top 100 Most deadly city. I think I found prices law hiding in the data(not really suprised)

--- Data/Graphs.R
+++ Data/Graphs.R
@@ -1,7 +1,19 @@
 #Bar plot on deaths per state
+byStates <- sort(table(data$state),  decreasing = TRUE)
+
+barplot(byStates, horiz = FALSE, las=2, border=NA)
+
+#Bar plot with top ten most deaths by state
+barplot(byStates[0:10], horiz = FALSE, las=2, border=NA)
 
 #Bar plot with top ten most deaths by city
-#Bar plot with top ten most deaths by city
+byCity <- sort(table(data$city_or_county), decreasing = TRUE)
+
+barplot(byCity[0:100], horiz = FALSE, las=2, border=NA)
+
+#Prices Law? 10% of places == 50% of deaths, looks plausable
+sum(byCity)
+sum(byCity[0:100])
 
 #lineplot with dates of shootings
 
